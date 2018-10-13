a5147880ef8968a4907b3e5327f9d84c46e56298
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-10-13 14:19:45 +0000

    Created a coins object and defined it

---
+++ coinsObject01.js
@@ -0,0 +1,10 @@
+let assignment = "Create an object for coins";
+let currencyAbbr = ["AUD", "GBP", "EUR", "JPY", "CHF", "USD", "AFN", "ALL", "DZD", "AOA"];
+
+let usd = {
+    penny: 0.01,
+    nickel: 0.05,
+    dime: 0.10,
+    quarter: 0.25,
+    halfDollar: 0.50
+};
