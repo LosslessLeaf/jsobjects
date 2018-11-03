09c047dfcf6932bb9034531dfefc85c2533c7f68
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-11-03 15:06:05 +0000

    updated hotel object

--- hotel-object/hotelInteractive.js
+++ hotel-object/hotelInteractive.js
@@ -10,7 +10,7 @@ vacant[0] = {
         hottub: "No Hot tub",
     },
     occupancy: 1,
-    availability: "Currently Available",
+    availability: "Available",
     price: 60
 };
 vacant[1] = {
@@ -23,7 +23,7 @@ vacant[1] = {
         hottub: "No Hot tub"
     },
     occupancy: 2,
-    availability: "Currently Available",
+    availability: "Available",
     price: 100
 };
 vacant[2] = {
@@ -36,7 +36,7 @@ vacant[2] = {
         hottub: "No Hot tub"
     },
     occupancy: 4,
-    availability: "Currently Available",
+    availability: "Available",
     price: 250
 };
 vacant[3] = {
@@ -49,47 +49,76 @@ vacant[3] = {
         hottub: "Hot tub"
     },
     occupancy: 6,
-    availability: "Currently Available",
+    availability: "Available",
     price: 1000
 
 };
 
 
-function populate() {
-    var roomName = document.getElementById("common-room").value;
-    for (let i = 0; i < vacant.length; i++) {
-        roomName.innerHTML = vacant[i].room;
-    }
+// function populate() {
+//     var roomName = document.getElementById("common-room").value;
+//     for (let i = 0; i < vacant.length; i++) {
+//         roomName.innerHTML = vacant[i].room;
+//     }
 
-    // if (Room.classList.contains("room-name")) {
-    //     Room.classList.remove("room-name");
-    //     Room.classList.add("room-name-displayed");
-    // }
-    // else {
-    //     Room.classList.remove("room-name-displayed");
-    //     Room.classList.add("room-name");
-    // }
-}
+//     // if (Room.classList.contains("room-name")) {
+//     //     Room.classList.remove("room-name");
+//     //     Room.classList.add("room-name-displayed");
+//     // }
+//     // else {
+//     //     Room.classList.remove("room-name-displayed");
+//     //     Room.classList.add("room-name");
+//     // }
+// }
 
 function selectedRoom() {
+    var displayProperty = document.getElementById("chosen-room-display");
     var chosenRoom = document.getElementById("hotel-room").value;
+    if (displayProperty.classList.contains("no-display")) {
+        displayProperty.classList.remove("no-display");
+        displayProperty.classList.add("display");
+    }
+
+
     for (let i = 0; i < vacant.length; i++) {
         if (chosenRoom == vacant[i].room) {
-            document.getElementById("chosen-room").innerText = 
-`${chosenRoom}`;
-        document.getElementById("amenities").innerText = `Amenities:`;
-        document.getElementById("amen1").innerText = vacant[i].amenities.mattress;
-        document.getElementById("amen2").innerText = vacant[i].amenities.smoking;
-        document.getElementById("amen3").innerText = vacant[i].amenities.wifi;
-        document.getElementById("amen4").innerText = vacant[i].amenities.hottub;
-        document.getElementById("occupancy").innerText = vacant[i].occupancy;
-        document.getElementById("availability").innerText = vacant[i].availability;
-        document.getElementById("price").innerText = vacant[i].price;
+            document.getElementById("chosen-room").innerText = vacant[i].room;
+            document.getElementById("amenities").innerText = `Amenities:`;
+            document.getElementById("amen1").innerText = vacant[i].amenities.mattress;
+            document.getElementById("amen2").innerText = vacant[i].amenities.smoking;
+            document.getElementById("amen3").innerText = vacant[i].amenities.wifi;
+            document.getElementById("amen4").innerText = vacant[i].amenities.hottub;
+            document.getElementById("occupancy").innerText = `Occupancy: ${vacant[i].occupancy}`;
+            document.getElementById("availability").innerText = `Currently: ${vacant[i].availability}`;
+            document.getElementById("price").innerText = `Price per night: ${vacant[i].price}`;
+        }
+    }
+
+
+}
+
+function finalizedRoom() {
+    var displayProperty = document.getElementById("redisplay");
+    var finalRoom = document.getElementById("final-room");
+    var chosenRoom = document.getElementById("hotel-room").value;
+    
+    
+
+    if (finalRoom.classList.contains("no-display")) {
+        
+        displayProperty.classList.remove("display");
+        displayProperty.classList.add("no-display");
+        finalRoom.classList.add("display");
+    
+        for (let i = 0; i < vacant.length; i++) {
+            if (chosenRoom == vacant[i].room) {
+                document.getElementById("confirmed").innerText = `You have confirmed: ${vacant[i].room}`;
+                document.getElementById("final-room-confirmed").innerText = `Thanks for choosing the Astoria Inn, and we hope you enjoy your stay!`;
+            }
         }
     }
 
 }
-
 
 function validateForm() {
     var x = document.forms["myForm"]["fname"].value;
